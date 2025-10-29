46c432141f5c316c2cf61727128fce0c3912837a
Zenable Automation <automation@zenable.io>
2025-10-29 11:29:24 -0400

    feat(project): initial project generation

---
+++ src/triss25/__init__.py
@@ -0,0 +1,8 @@
+"""
+triss25 init
+"""
+
+__maintainer__ = "jonzeolla"
+__license__ = "MIT"
+__project_name__ = "triss25"
+__version__ = "0.0.0"
---
+++ src/triss25/constants.py
@@ -0,0 +1,14 @@
+"""
+triss25 constants
+"""
+
+import json
+
+LOG_FORMAT = json.dumps(
+    {
+        "timestamp": "%(asctime)s",
+        "namespace": "%(name)s",
+        "loglevel": "%(levelname)s",
+        "message": "%(message)s",
+    }
+)
